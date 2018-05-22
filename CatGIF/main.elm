b0a09255009ba13a1e3e32d2a06178c623ac35bd
Keshav Sharma <kesharma@TSTK.local>
2018-05-22 15:04:37 +0530

    first commit

---
+++ CatGIF/main.elm
@@ -0,0 +1,67 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
+
+main =
+  Html.program
+    { init =init "cats"
+      ,view=view
+      ,update=update
+      ,subscriptions=subscriptions
+    }
+
+-- MODEL
+type alias Model =
+  { topic : String
+    , gifUrl : String
+  }
+
+--INIT
+init: String -> (Model, Cmd Msg)
+init topic =
+  (Model "cats" "waiting.gif", getRandomGif topic)
+
+--UPDATE
+type Msg
+  = MorePlease
+  | NewGif (Result Http.Error String)
+update:Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    MorePlease ->
+      (model, getRandomGif model.topic)
+    NewGif(Ok newUrl) ->
+      ({model | gifUrl=newUrl}, Cmd.none)
+    NewGif(Err _) ->
+      (model , Cmd.none)
+
+--VIEW
+view: Model->Html Msg
+view model=
+  div[]
+  [
+    h2[][text model.topic]
+    , img[src model.gifUrl][]
+    , button[onClick MorePlease][ text "More Gif please"]
+  ]
+
+
+--Get random gif
+getRandomGif: String -> Cmd Msg
+getRandomGif topic=
+  let
+    url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+  in
+    Http.send NewGif (Http.get url decodeGifUrl)
+
+-- Decode gif Url
+decodeGifUrl: Decode.Decoder String
+decodeGifUrl=
+  Decode.at["data","image_url"] Decode.string
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
